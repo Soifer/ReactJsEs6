2ce4a526a44c140d567a5966105d4863c4af49dc
Mark Soifer <posurlu@gmail.com>
2017-04-20 13:01:09 +0300

    inputs created

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -9,10 +9,24 @@ const TextInput = ({
     error
 }) => {
     let wrapperClass = 'form-group';
+    if(error && error.length > 0){
+        wrapperClass += " " + 'has-error';
+    }
     return (
-        <div className={wrapperClass}></div>
+        <div className={wrapperClass}>
+            <label htmlFor="{name}">{label}</label>
+            <div className="field">
+                <input
+                 type="text"
+                 name={name}
+                 className="form-control"
+                 placeholder={placeholder}
+                 value={value}
+                 onChange={onChange}/>
+                 {error && <div className="alert alert-danger">{error}</div>}
+            </div>
+        </div>
     );
-
 };
 
 TextInput.propTypes = {
