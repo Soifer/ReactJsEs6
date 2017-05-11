c346717ca957e70e61ce160dddba8db02dfbd21e
Mark Soifer <posurlu@gmail.com>
2017-05-11 11:46:12 +0300

    Enzyme test added

--- src/components/course/CourseForm.ReactTestUtils.test.js
+++ src/components/course/CourseForm.ReactTestUtils.test.js
@@ -3,9 +3,11 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import CourseForm from './CourseForm';
 
-function setup(saving){
+function setup(saving) {
     let props = {
-        course: {}, saving: saving, errors: {},
+        course: {},
+        saving: saving,
+        errors: {},
         onSave: () => {},
         onChange: () => {}
     };
@@ -14,24 +16,26 @@ function setup(saving){
     renderer.render(<CourseForm {...props}/>);
     let output = renderer.getRenderOutput();
 
-    return {
-        props,
-        output,
-        renderer
-    };
+    return {props, output, renderer};
 }
 
-describe('CourseForm via React Test Utils', () =>{
+describe('CourseForm via React Test Utils', () => {
     it('renders form and h1', () => {
         const {output} = setup();
         expect(output.type).toBe('form');
         let [h1] = output.props.children;
-        expect( h1.type).toBe('h1');
+        expect(h1.type).toBe('h1');
     });
 
-    it('save button is labled "Save" when not saving', () =>{
+    it('save button is labled "Save" when not saving', () => {
         const {output} = setup(false);
         const submitButton = output.props.children[5];
         expect(submitButton.props.value).toBe('Save');
+    });
+
+    it('save button is labled "Saving..." when not saving', () => {
+        const {output} = setup(true);
+        const submitButton = output.props.children[5];
+        expect(submitButton.props.value).toBe('Saving...');
     });
 });
